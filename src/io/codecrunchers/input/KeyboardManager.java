86b0c15b55bae1180c6b36e49ee8d307665acaed
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-05 17:45:16 +1100

    - Created Placeholder KeyboardManager Class file

---
+++ src/io/codecrunchers/input/KeyboardManager.java
@@ -0,0 +1,21 @@
+package io.codecrunchers.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardManager implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
