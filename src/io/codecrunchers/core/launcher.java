8db4a315ee4c985db75c4c00efc326cc3eceaaf7
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-05 17:52:56 +1100

    - Updated launcher to create game instance

--- src/io/codecrunchers/core/launcher.java
+++ src/io/codecrunchers/core/launcher.java
@@ -2,9 +2,9 @@ package io.codecrunchers.core;
 
 public class launcher {
 
+
     public static void main(String[] args)
     {
-        System.out.println("This is a github test commit2!");
-
+        new Game(480,360,"Code Crunchers Development Build");
     }
 }
