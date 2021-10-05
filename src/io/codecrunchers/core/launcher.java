5cf9ef1f81f4f81aadb67d79b8a45855db038be8
SeanAth017 <sean@atho.com.au>
2021-10-05 17:50:59 +1100

    -Updated Comment

--- src/io/codecrunchers/core/launcher.java
+++ src/io/codecrunchers/core/launcher.java
@@ -5,6 +5,7 @@ public class launcher {
 
     public static void main(String[] args)
     {
+        //create new instance of game object.
         new Game(480,360,"Code Crunchers Development Build");
     }
 }
