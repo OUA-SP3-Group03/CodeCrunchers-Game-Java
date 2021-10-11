abc15e1295e52bdeaf68332f4a19e2c5a32afeb9
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-11 16:13:02 +1100

    - Created main application class file, stores the core game application methods and interfaces with the Kernal and Service Providers via the app facade.

---
+++ src/io/codecrunchers/core/Application.java
@@ -0,0 +1,59 @@
+package io.codecrunchers.core;
+
+import io.codecrunchers.facades.App;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Application {
+
+    private final App app;
+
+    public Application(App app){
+        //set this app equal to the app facade
+        this.app = app;
+        //Console log application creation
+        System.out.println("Application Created!");
+
+        //render and tick testing
+        while(true) {
+            this.render();
+            this.tick();
+        }
+    }
+
+    public void tick(){
+        this.app.tick();
+    }
+
+    public void render(){
+        //create buffered strategy for the app canvas
+        BufferStrategy bs = this.app.canvas().getBufferStrategy();
+            if(bs == null){
+                this.app.canvas().createBufferStrategy(3);
+                return;
+            }
+
+       //create the graphics object to draw to that buffered strategy
+       Graphics g = bs.getDrawGraphics();
+
+       //__ START RENDER
+
+           //PLACEHOLDER TESTING
+           g.clearRect(0,0,400,400);
+           g.drawString("Hello World!",20,20);
+           g.setColor(Color.red);
+           g.drawRect(50,50,100,100);
+
+           //render providers
+            this.app.render(g);
+
+
+       //__ END RENDER
+
+       //finally show the buffered strategy and dispose of the old graphics
+       bs.show();
+       g.dispose();
+    }
+
+}
