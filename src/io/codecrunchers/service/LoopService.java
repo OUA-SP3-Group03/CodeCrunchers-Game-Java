f47255aeb2ed1e4a2de94bc618971d0f2dc655f0
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-11 17:14:44 +1100

    - Created main LoopService class to manage the loop

---
+++ src/io/codecrunchers/service/LoopService.java
@@ -0,0 +1,85 @@
+package io.codecrunchers.service;
+
+import io.codecrunchers.providers.LoopServiceProvider;
+
+public class LoopService implements Runnable{
+
+    private final LoopServiceProvider loopServiceProvider;
+    private Thread thread;
+
+    public LoopService(LoopServiceProvider loopServiceProvider){
+        this.loopServiceProvider = loopServiceProvider;
+    }
+
+    @Override
+    public void run() {
+
+        //TEMP local vars
+        int fps = 60;
+        double timePerTick = 1000000000 /fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        int ticks = 0;
+
+        while(this.loopServiceProvider.getRunningStatus()){
+            now = System.nanoTime();
+            delta+=(now-lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+
+            if(delta >= 1){
+
+                this.loopServiceProvider.getApplication().render();
+                this.loopServiceProvider.getApplication().tick();
+
+                ticks++;
+                delta--;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("fps: "+ticks);
+                ticks = 0;
+                timer = 0;
+            }
+        }
+    this.stop();
+    }
+
+    public synchronized void start(){
+        if(this.loopServiceProvider.getRunningStatus()){
+            return;
+        }else{
+            System.out.println("starting loop");
+            this.loopServiceProvider.setRunningStatus(true);
+            this.thread = new Thread(this);
+            this.thread.start();
+        }
+    }
+
+    public synchronized void stop(){
+        if(!this.loopServiceProvider.getRunningStatus()){
+            return;
+        }
+        else{
+            try{
+                System.out.println("stopping loop");
+                this.thread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    //TEMP
+    private void render(){
+       // System.out.println("render fired");
+    }
+
+    private void tick(){
+        //System.out.println("tick fired");
+    }
+}
