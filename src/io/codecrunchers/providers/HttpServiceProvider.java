e1900638c9861b60f2f806f287f5e1d1ee38657d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-11 16:16:37 +1100

    - Created placeholder HttpServiceProvider

---
+++ src/io/codecrunchers/providers/HttpServiceProvider.java
@@ -0,0 +1,32 @@
+package io.codecrunchers.providers;
+
+import io.codecrunchers.core.Provider;
+
+import java.awt.*;
+
+public class HttpServiceProvider extends Provider {
+    @Override
+    public void boot() {
+        //TODO add service provider code
+    }
+
+    @Override
+    public boolean performTick() {
+        return false;
+    }
+
+    @Override
+    public boolean performRender() {
+        return false;
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
