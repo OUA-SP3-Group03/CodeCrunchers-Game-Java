3f47d4d7f1029be1364a8fd2b396f5e038d5e73b
Bruno-s3802201 <s3802201@student.rmit.edu.au>
2021-10-14 14:28:44 +1100

    -boot display first + if statement to prevent double boot

--- src/io/codecrunchers/core/Kernel.java
+++ src/io/codecrunchers/core/Kernel.java
@@ -56,13 +56,21 @@ public class Kernel {
     }
 
     public void bootProviders(){
-        //run boot method on all service providers
+        //Display how many service providers are registered
         System.out.println(this.providers.size()+" Service Providers Registered");
+
+        //Boot display first to avoid any boot errors
+        this.providers.get("display").boot(this.app);
+
+        //run boot method on all service providers
         for(Provider provider : this.providers.values()){
             //display what provider is being booted in console
             System.out.println("Booting : "+provider);
-            //boot provider
-            provider.boot(this.app);
+            //if provider isn't already booted
+            if(!provider.isBooted()) {
+                //boot provider
+                provider.boot(this.app);
+            }
         }
 
         this.booted = true;
