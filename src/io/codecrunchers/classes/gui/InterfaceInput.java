db3e0afcbcd73fb707164c4b2c8158d1df70aff0
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-14 15:41:59 +1100

    - Created Placeholder InterfaceInput class

---
+++ src/io/codecrunchers/classes/gui/InterfaceInput.java
@@ -0,0 +1,15 @@
+package io.codecrunchers.classes.gui;
+
+import java.awt.*;
+
+public class InterfaceInput extends InterfaceObject{
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
