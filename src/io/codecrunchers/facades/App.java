52557253e5456a4d6b90da4372703a58430332fa
SeanAth017 <sean@atho.com.au>
2021-10-18 15:04:19 +1100

    - Added Texture method - Added images getter

--- src/io/codecrunchers/facades/App.java
+++ src/io/codecrunchers/facades/App.java
@@ -2,16 +2,32 @@ package io.codecrunchers.facades;
 
 import io.codecrunchers.core.Application;
 import io.codecrunchers.core.Kernel;
+import io.codecrunchers.providers.AssetServiceProvider;
 import io.codecrunchers.providers.DisplayServiceProvider;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class App {
 
     private final Kernel kernel;
 
+    private Texture texture;
+
     public App(Kernel kernel){
         this.kernel = kernel;
+    }
+
+    public void textureCallback(Texture texture){
+        this.texture = texture;
+    }
+    public Texture texture (){
+        return this.texture;
+    }
+
+//Testing
+    public BufferedImage image(){
+        return ((AssetServiceProvider) this.kernel.getServiceProvider("asset")).getTest();
     }
 
     //**** GET INSTANCE OF THE APPLICATION ****\\
@@ -52,6 +68,24 @@ public class App {
     //**** GET TARGET FPS & TPS ****\\
     public int targetFPS(){
         return this.kernel.getConfig().targetFPS;
+    }
+
+    public int textureWidth(){
+        return this.kernel.getConfig().textureWidth;
+    }
+
+    public int textureHeight(){
+        return this.kernel.getConfig().textureHeight;
+    }
+    public int textureMapWidth(){
+        return this.kernel.getConfig().textureMapWidth;
+    }
+    public int textureMapHeight(){
+        return this.kernel.getConfig().textureMapHeight;
+    }
+
+    public String texturePath(){
+        return this.kernel.getConfig().texturePath;
     }
 
 }
