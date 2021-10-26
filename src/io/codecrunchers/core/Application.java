a67e2c2ae89886cca03367d378a18f79b20bf639
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-26 14:31:13 +1100

    - Removed all debug code from application and add registration for our states as well as setting the current state to login and initializing the states.

--- src/io/codecrunchers/core/Application.java
+++ src/io/codecrunchers/core/Application.java
@@ -1,16 +1,11 @@
 package io.codecrunchers.core;
 
-import io.codecrunchers.classes.gui.InterfaceButton;
-import io.codecrunchers.classes.states.GameState;
-import io.codecrunchers.classes.states.SettingsState;
-import io.codecrunchers.classes.gui.InterfaceCheckBox;
-import io.codecrunchers.classes.gui.InterfaceInput;
-import io.codecrunchers.classes.states.State;
+import io.codecrunchers.classes.states.*;
+
 import io.codecrunchers.facades.App;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
-import java.io.IOException;
 
 public class Application {
 
@@ -19,68 +14,23 @@ public class Application {
     public void onBootCompletion(App app) {
         //set this app equal to the app facade
         this.app = app;
+
+        //register the states
+        this.app.registerState("login", new LoginState());
+        this.app.registerState("game", new GameState());
+        this.app.registerState("menu", new MenuState());
+        this.app.registerState("pause", new PauseState());
+        this.app.registerState("settings", new SettingsState());
+
+        //initialize the states
+        this.app.initializeStates();
+
+        //set the default starting state
+        this.app.setCurrentState("login");
+
         //start the main loop
         this.app.startLoop();
 
-        //DEBUGGING INTERFACE OBJECT
-        //button one
-        this.app.addInterfaceObject(new InterfaceInput()
-                .setAppFacade(this.app)
-                .setX(this.app.config().interfaceWidth()/2-(this.app.config().textureWidth()*3)*this.app.config().textureScale())
-                .setY(this.app.config().interfaceHeight()/2 - this.app.config().textureHeight()*this.app.config().textureScale())
-                .setWidth(this.app.config().textureWidth()*6)
-                .setHeight(this.app.config().textureHeight())
-                .setTextAlign("left")
-                .setText("Email"));
-
-        this.app.addInterfaceObject(new InterfaceInput()
-                .setAppFacade(this.app)
-                .setX(this.app.config().interfaceWidth()/2-(this.app.config().textureWidth()*3)*this.app.config().textureScale())
-                .setY((int) (this.app.config().interfaceHeight()/2+ this.app.config().textureHeight()*this.app.config().textureScale()*0.5))
-                .setWidth(this.app.config().textureWidth()*6)
-                .setHeight(this.app.config().textureHeight())
-                .setHiddenInput(true)
-                .setTextAlign("left")
-                .setText("Password"));
-
-        this.app.addInterfaceObject(new InterfaceCheckBox()
-                .setAppFacade(this.app)
-                .setX(this.app.config().interfaceWidth()/2- (this.app.config().textureWidth()*3)*this.app.config().textureScale())
-                .setY((this.app.config().interfaceHeight()/2 + this.app.config().textureHeight() *2* this.app.config().textureScale()-14))
-                .setWidth(this.app.config().textureWidth()*2)
-                .setHeight(this.app.config().textureHeight())
-                .setShowOnHover(false)
-                .setClickEvent(()-> System.out.println("toggled")));
-
-
-        this.app.addInterfaceObject(new InterfaceButton()
-                .setAppFacade(this.app)
-                .setX(this.app.config().interfaceWidth()/2-this.app.config().textureWidth()*3*this.app.config().textureScale())
-                .setY((this.app.config().interfaceHeight()/2 + this.app.config().textureHeight()*3 * this.app.config().textureScale()))
-                .setWidth(this.app.config().textureWidth()*3)
-                .setHeight(this.app.config().textureHeight())
-                .setState(new State())
-                .setText("Login")
-                .setHoverBoxCurve(22)
-                .setClickEvent(()-> System.out.println("Clicked Login")));
-
-        //button two
-        this.app.addInterfaceObject(new InterfaceButton()
-                .setAppFacade(this.app)
-                .setX(this.app.config().interfaceWidth()/2+4)
-                .setY((this.app.config().interfaceHeight()/2 + this.app.config().textureHeight()*3 * this.app.config().textureScale()))
-                .setWidth(this.app.config().textureWidth()*3)
-                .setText("Register")
-                .setHeight(this.app.config().textureHeight())
-                .setState(new State())
-                .setHoverBoxCurve(22)
-                .setClickEvent(()-> {
-                    try {
-                        Desktop.getDesktop().browse(java.net.URI.create("http://codecrunchers.io/register"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }));
     }
 
     public void tick(){
@@ -99,22 +49,12 @@ public class Application {
        //create the graphics object to draw to that buffered strategy
        Graphics g = bs.getDrawGraphics();
 
+       //clear the screen
        g.clearRect(0,0,this.app.config().interfaceWidth(),this.app.config().interfaceHeight());
 
-       //__ START RENDER
-        g.setColor(Color.white);
-        g.fillRect(0,0,this.app.config().interfaceWidth(),this.app.config().interfaceHeight());
+        //render all providers
+       this.app.render(g);
 
-            Font currentFont = g.getFont();
-            Font newFont = currentFont.deriveFont(currentFont.getSize() * 1.4F);
-            g.setFont(newFont);
-            g.setColor(Color.black);
-
-            g.drawImage(app.texture().logo(),0,64,null);
-
-        g.drawString("Keep me logged in",this.app.config().interfaceWidth()/2-55,this.app.config().interfaceHeight()/2 + this.app.config().textureHeight() *2* this.app.config().textureScale()+24);
-           //render providers
-           this.app.render(g);
 
        //__ END RENDER
 
