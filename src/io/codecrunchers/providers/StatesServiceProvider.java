fd8ce433ed36bb3642d5934b795cb8891b6ff1ed
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-10-26 14:33:03 +1100

    - Merged GSM into StatesServiceProvider and added getters and setters for new states and the current state variable.

--- src/io/codecrunchers/providers/StatesServiceProvider.java
+++ src/io/codecrunchers/providers/StatesServiceProvider.java
@@ -1,49 +1,79 @@
 package io.codecrunchers.providers;
 
+import io.codecrunchers.classes.states.State;
 import io.codecrunchers.core.Provider;
 import io.codecrunchers.facades.App;
 import java.awt.*;
+import java.util.HashMap;
 
 public class StatesServiceProvider extends Provider {
-    //______ REMEMBER TO REGISTER PROVIDER WITH KERNEL ______\\
-    // refer to notes in kernel.class for instructions
+
+    private HashMap<String,State> states;
+
+    private String currentState;
+    private App app;
 
     //**** BOOT METHOD ****\\
     @Override
     public void boot(App app) {
-
-        //this method is called by the Kernel when the program loads for the first time, you can think of this like your constructor class
-        //place any code that you need to run at the start of the program once in here
+        this.states = new HashMap<>();
+        this.app = app;
     }
 
     //**** PERFORM TICK METHOD ****\\
     @Override
     public boolean performTick() {
         //this method tells the kernel if this service provider needs to be ticked or not, return true for yes or false for no
-        return false;
+        return true;
     }
 
     //**** PERFORM RENDER METHOD ****\\
     @Override
     public boolean performRender() {
         //this method tells the kernel if this service provider needs to render code in its render function, return true for yes or no for false.
-        return false;
+        return true;
     }
 
     //**** RENDER METHOD ****\\
     @Override
     public void render(Graphics g) {
-        //place all your canvas rendering code in this method, this is called by the kernel at a time specified by the program max frames per second
-        //keep this code as lean as possible to keep performance high
+        if(this.currentState != null) {
+            this.states.get(this.currentState).render(g);
+        }
     }
 
     //**** TICK METHOD ****\\
     @Override
     public void tick() {
-        //place all your tick or update code in this method, this is called by the kernel at a time specified by the max ticks per second
-        //keep this code as lean as possible to keep performance high
+        if(this.currentState != null) {
+            this.states.get(this.currentState).tick();
+        }
     }
 
-    //**** YOUR METHOD ****\\
+    public void registerState(String key, State state){
+            this.states.put(key, state);
+    }
+
+    public void setCurrentState(String key){
+        this.currentState = key;
+    }
+
+    public String getCurrentState(){
+        return this.currentState;
+    }
+
+    public boolean checkStateKey(String key){
+        return this.states.containsKey(key);
+    }
+
+    public void initialize(){
+        for (State state: this.states.values()) {
+                state.boot(this.app);
+        }
+    }
+
+
+
+
 
 }
