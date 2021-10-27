5fd5d4c0134ada588131a0cb2a18a8967855cdac
Bruno-s3802201 <s3802201@student.rmit.edu.au>
2021-10-27 14:11:33 +1100

    -changed height to 704 -textureMap_v5

--- src/io/codecrunchers/core/Config.java
+++ src/io/codecrunchers/core/Config.java
@@ -3,14 +3,14 @@ package io.codecrunchers.core;
 public class Config {
     public String title = "Code Crunchers";
     public int interfaceWidth = 1280;
-    public int interfaceHeight = 720;
+    public int interfaceHeight = 704;
     public int targetFPS = 60;
 
     public boolean showFPS = false;
     public int targetTPS = 20;
     public String apiUrl = "http://localhost/api";
   
-    public String texturePath = "res/textureMap_v2.png";
+    public String texturePath = "res/textureMap_v5.png";
     public String logoPath = "res/CodeCrunchers-Logo.png";
     public int textureWidth = 32;
     public int textureHeight = 32;
