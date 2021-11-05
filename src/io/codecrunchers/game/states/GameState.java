e9b0d09c191fda47d2f3508479a80fd0132c04be
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-05 16:50:52 +1100

    - Updated gameState to use new level generator code

--- src/io/codecrunchers/game/states/GameState.java
+++ src/io/codecrunchers/game/states/GameState.java
@@ -15,29 +15,11 @@ public class GameState extends State {
     @Override
     public void boot(App app) {
         this.app = app;
-        int[] World;
-        String worldRaw="10000222000000000001" +
-                        "00000000000000000000" +
-                        "00000000000000000000" +
-                        "00000000000000000000" +
-                        "00000000000222200000" +
-                        "00000000000000000000" +
-                        "00022222200000000000" +
-                        "00000000000220220220" +
-                        "00000022220000000000" +
-                        "00000000000000000000" +
-                        "11111111111111111111";
-        int worldWidth=20;
-        int worldHeight=11;
-        World=new int[worldRaw.length()];
 
-        for(int i=0;i<worldRaw.length();i++){
-            World[i]=worldRaw.charAt(i)-'0';
-        }
-
-        this.app.setWorldWidth(worldWidth);
-        this.app.setWorldHeight(worldHeight);
-        this.app.setWorld(World);
+        this.app.level().generate();
+        this.app.setWorldWidth(this.app.level().width());
+        this.app.setWorldHeight(this.app.level().height());
+        this.app.setWorld(this.app.level().tiles());
 
         player = new Player(64, 0, 64, 64, this.app);
         this.app.registerEntity(player);
@@ -46,7 +28,10 @@ public class GameState extends State {
     public void tick() {
         if(this.app.keyPressed().containsKey(ASCII.escape)){
             this.app.keyPressed().remove(ASCII.escape);
-            this.app.setCurrentState("pause");
+            this.app.level().generate();
+            this.app.setWorldWidth(this.app.level().width());
+            this.app.setWorldHeight(this.app.level().height());
+            this.app.setWorld(this.app.level().tiles());
         }
 
         this.app.getCamera().centerOnEntity(player);
