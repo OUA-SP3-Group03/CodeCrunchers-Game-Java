b60dce611f72957795d074f1c2751fb683f52ac2
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-13 16:07:00 +1100

    - Removed Duplicate code from AudioService.

--- src/io/codecrunchers/service/AudioService.java
+++ src/io/codecrunchers/service/AudioService.java
@@ -6,39 +6,15 @@ import java.io.File;
 public class AudioService {
 
     private Clip clip;
-    private String path;
-    private float volume;
+    private final String path;
+    private final float volume;
 
     public AudioService(String path, float volume){
 
         this.path = path;
         this.volume = volume;
 
-        try{
-            AudioInputStream stream = AudioSystem.getAudioInputStream(new File(path));
-            AudioFormat format = stream.getFormat();
-
-            if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
-                format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, format.getSampleRate(),
-                        format.getSampleSizeInBits() * 2, format.getChannels(), format.getFrameSize() * 2,
-                        format.getFrameRate(), true);
-                stream = AudioSystem.getAudioInputStream(format, stream);
-            }
-
-            DataLine.Info info = new DataLine.Info(Clip.class, stream.getFormat(),
-                    ((int) stream.getFrameLength() * format.getFrameSize()));
-
-            clip = (Clip) AudioSystem.getLine(info);
-
-            clip.open(stream);
-
-            //Volume control
-            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            volumeControl.setValue(volume);
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        this.reset();
 
     }
 
