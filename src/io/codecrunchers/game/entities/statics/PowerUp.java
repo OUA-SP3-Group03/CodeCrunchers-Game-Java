1e2006bd8782631f55f4d28c51ed3ebf4336cbd3
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-14 16:51:49 +1100

    - Added Audio play to collsion. - Reset audio on death.

--- src/io/codecrunchers/game/entities/statics/PowerUp.java
+++ src/io/codecrunchers/game/entities/statics/PowerUp.java
@@ -9,7 +9,7 @@ public class PowerUp extends StaticEntity {
     public PowerUp(float x, float y, App app) {
         super(x, y);
         this.app = app;
-        this.texture = this.app.texture().allImages()[34];
+        this.texture = this.app.texture().allImages()[26];
     }
 
     @Override
@@ -19,25 +19,28 @@ public class PowerUp extends StaticEntity {
     @Override
     public void render(Graphics g) {
         if(this.app.showDebug()){
+            g.setColor(Color.pink);
             g.drawRect((int) ((int)this.x- this.app.getCamera().getxOffset()),(int)this.y,this.width,this.height);
         }
         g.drawImage(this.texture, (int) ((int)x- this.app.getCamera().getxOffset()),(int)y,null);
     }
 
-    @Override
-    public boolean isAlive() {
-        return true;
-    }
 
     @Override
     public void die() {
-
+        this.app.resetAudioClip("powerup");
+        this.setAlive(false);
     }
 
     @Override
     public Rectangle getBounds() {
-        return null;
+        return new Rectangle((int)this.x,(int)this.y,this.width,this.height);
     }
 
 
+    @Override
+    public void collisionWithPlayer() {
+        this.app.playAudioClip("powerup");
+        this.die();
+    }
 }
