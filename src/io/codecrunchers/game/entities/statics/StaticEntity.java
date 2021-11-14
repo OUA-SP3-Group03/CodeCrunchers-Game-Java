3817787a7e17ac237f0fb1aa2645faac6953f938
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-14 14:40:51 +1100

    - Updated StaticEntity to use 64x64 hard coded size.

--- src/io/codecrunchers/game/entities/statics/StaticEntity.java
+++ src/io/codecrunchers/game/entities/statics/StaticEntity.java
@@ -4,7 +4,7 @@ import io.codecrunchers.game.entities.Entity;
 
 public abstract class StaticEntity extends Entity {
 
-    public StaticEntity(float x, float y, int width, int height) {
-        super(x, y, width, height);
+    public StaticEntity(float x, float y) {
+        super(x, y, 64, 64);
     }
 }
