ac660a07157671933fccc704b433bbaf4c72360c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-14 14:35:25 +1100

    - Renamed RoofTile to InteriorTile.

--- src/io/codecrunchers/game/tiles/InteriorTile.java
+++ src/io/codecrunchers/game/tiles/InteriorTile.java
@@ -3,19 +3,18 @@ package io.codecrunchers.game.tiles;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class RoofTile extends Tile{
-    public RoofTile(BufferedImage texture) {
+public class InteriorTile extends Tile{
+    public InteriorTile(BufferedImage texture) {
         super(texture);
     }
 
     @Override
-    public void render(Graphics g, int x, int y) {
+    public void render(Graphics g, int x, int y, int tileX, int tileY) {
         g.drawImage(this.texture,x,y,null);
-
     }
 
     @Override
     public boolean solid() {
-        return true;
+        return false;
     }
 }
