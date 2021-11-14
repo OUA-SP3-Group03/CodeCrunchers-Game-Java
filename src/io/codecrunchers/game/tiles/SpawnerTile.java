a34a51854040dbf8be45c839b983afd74f31bde1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-14 14:40:29 +1100

    - Renamed ScaffoldingTile.java to SpawnerTile.

--- src/io/codecrunchers/game/tiles/SpawnerTile.java
+++ src/io/codecrunchers/game/tiles/SpawnerTile.java
@@ -1,21 +1,23 @@
 package io.codecrunchers.game.tiles;
 
+import io.codecrunchers.facades.App;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class ScaffoldingTile extends Tile{
-    public ScaffoldingTile(BufferedImage texture) {
+public class SpawnerTile extends Tile{
+
+    public SpawnerTile(BufferedImage texture) {
         super(texture);
     }
 
     @Override
-    public void render(Graphics g, int x, int y) {
+    public void render(Graphics g, int x, int y, int tileX, int tileY) {
         g.drawImage(this.texture,x,y,null);
-
     }
 
     @Override
     public boolean solid() {
-        return true;
+        return false;
     }
 }
