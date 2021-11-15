80b65d0edbcab87d97deb85922e26bf12baf8b64
Jaejun-OH <s3905545@student.rmit.edu.au>
2021-11-15 19:05:27 +0900

    + minor updates in method implementation

--- src/io/codecrunchers/game/entities/creatures/enemies/Enemy.java
+++ src/io/codecrunchers/game/entities/creatures/enemies/Enemy.java
@@ -1,10 +1,38 @@
 package io.codecrunchers.game.entities.creatures.enemies;
 
+import io.codecrunchers.facades.App;
 import io.codecrunchers.game.entities.creatures.Creature;
+
+import java.awt.*;
 
 public abstract class Enemy extends Creature {
 
-    public Enemy(float x, float y, int width, int height) {
-        super(x, y, width, height);
+
+    protected int facing;
+
+    public Enemy(float x, float y, int width, int height, App app) {
+        super(x, y, width, height, app);
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(this.texture, (int) ((int) this.x - this.app.getCamera().getxOffset()), (int) ((int) this.y - this.app.getCamera().getyOffset()), null);
+    }
+
+
+
+
+
+
+
+
+    public void setFacing(int facing) {
+        this.facing = facing;
     }
 }
