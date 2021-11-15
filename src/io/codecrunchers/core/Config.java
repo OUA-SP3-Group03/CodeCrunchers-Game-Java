77903893e0dff1514d14352dc5053e1602bb26ab
Bruno-s3802201 <s3802201@student.rmit.edu.au>
2021-11-15 12:57:50 +1100

    -Changed Config URLs from "localhost" to "codecrunchers.io" (jack) -Added new textureMap version

--- src/io/codecrunchers/core/Config.java
+++ src/io/codecrunchers/core/Config.java
@@ -7,11 +7,11 @@ public class Config {
     public int targetFPS = 60;
     public int targetTPS = 60;
 
-    public String apiUrl = "http://localhost/api";
-    public String registerUrl = "http://localhost/register";
-    public String creditsUrl = "http://localhost/credits";
+    public String apiUrl = "https://codecrunchers.io/api";
+    public String registerUrl = "https://codecrunchers.io/register";
+    public String creditsUrl = "https://codecrunchers.io/credits";
 
-    public String texturePath = "res/Textures/textureMap_v6.1.png";
+    public String texturePath = "res/Textures/textureMap_v6.3.png";
     public String logoPath = "res/Textures/CodeCrunchers-Logo.png";
     public int textureWidth = 32;
     public int textureHeight = 32;
@@ -19,5 +19,4 @@ public class Config {
   
     public String world0path="res/WORLDS.txt";
     public int maxRooms=6;
-
 }
